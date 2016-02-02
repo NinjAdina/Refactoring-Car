9025dc3e1bfe8a76cedd2153acf15f3786833dc7
NinjAdina <AdinaShuttari@Adinas-MacBook-Pro.local>
2016-02-01 20:53:25 -0500

    Original code.

--- sketch.js
+++ sketch.js
@@ -1,53 +1,19 @@
-var car = {
-    x: 100,
-    y: 385,
-    bodyLength: 400,
-    bodyHeight: 100,
-    hoodLength: 115,
-    cabinLength: 200,
-    cabinHeight: 75,
-    carCorners:20,
-    frontWheelOffset: 65,
-    backWheelOffset: 65,
-    wheelBase: 270,
-    wheelWidth: 65,
-    wheelHeight: 65,
-
-    draw: function() {
-      this.drawBody();
-      this.drawCabin();
-      this.drawFrontWheel();
-      this.drawBackWheel();
-    },
-    drawBody: function() {
-      fill(color(0, 0, 255)); // Blue.
-      rect(this.x, this.y, this.bodyLength, this.bodyHeight, this.carCorners);
-    },
-
-    drawCabin: function() {
-      fill(color(0, 0, 255)); // Blue.
-      rect(this.x + this.hoodLength, this.y - this.cabinHeight, this.cabinLength, this.cabinHeight + this.carCorners, this.carCorners);
-    },
-
-    drawFrontWheel: function() {
-      fill(0);
-      ellipse(this.frontWheelOffset + this.x, this.y + this.bodyHeight, this.wheelWidth, this.wheelHeight);
-    },
-
-    drawBackWheel: function() {
-      fill(0);
-      ellipse(this.x + this.frontWheelOffset + this.wheelBase, this.y + this.bodyHeight, this.wheelWidth, this.wheelHeight);
-    }
-};
-
-var setup = function setup() {
+var setup = function setup () {
   createCanvas(600, 600); // The canvas.
   background(100); // It is a cloudy day.
   noStroke(); // Draw shapes without an outline.
   fill(0); // Black road.
   rect(0, 500, 600, 100); // The pavement
 
-  car.draw();
+  // The body of the car.
+  fill(color(0, 0, 255)); // Blue.
+  rect(100, 385, 400, 100, 20); // The main body.
+  rect(215, 325, 200, 100, 20); // The hood/windows part of the body.
+
+  // The wheels.
+  fill(0);
+  ellipse(165, 475, 65, 65); // The front tire on the left.
+  ellipse(435, 475, 65, 65); // The back tire on the right.
 
 }
 
